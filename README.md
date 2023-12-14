f445ff5a91c1182fdbc59e106aaceb41f58a0e09
MATTHIAS SCHOLZ <matthias.scholz@thoughtworks.com>
2023-12-14 14:15:03 +0100

    :tada: kickstarting template repo with go

---
+++ README.md
@@ -0,0 +1,37 @@
+# Templates
+
+Collection of templates to get started quickly and in a reproducible way.
+Leverages: direnv, nix
+
+## direnv with nix flakes
+
+``` sh
+# Use flake directly - composable
+echo "use flake \"github:the-nix-way/dev-templates?dir=flakes/go\"" >> .envrc
+direnv allow
+```
+
+``` sh
+# Use flake as a starting point - tailor
+nix flake init -t "github:the-nix-way/dev-templates#go"
+direnv allow
+```
+
+### Setup
+
+Using Nix Home Manager to install `nix-direnv`
+
+## Develop Templates
+
+- [ ] TODO Check
+
+``` sh
+cd flakes/go
+nix develop
+```
+
+
+## References
+
+- [Flakes inspired by](https://github.com/the-nix-way/dev-templates/tree/main)
+- [Use direnv with nix](https://determinate.systems/posts/nix-direnv)
---
+++ flakes/go/flake.nix
@@ -0,0 +1,32 @@
+# NOTE Copy from:https://github.com/the-nix-way/dev-templates/blob/main/go/flake.nix
+{
+  description = "A Nix-flake-based Go development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      goVersion = 21;
+      overlays = [ (final: prev: { go = prev."go_1_${toString goVersion}"; }) ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit overlays system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            # go (specified by overlay)
+            go
+
+            # goimports, godoc, etc.
+            gotools
+
+            # https://github.com/golangci/golangci-lint
+            golangci-lint
+          ];
+        };
+      });
+    };
+}

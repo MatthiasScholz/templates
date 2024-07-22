92528610174b4a1e480b1f3c52cf1116aee5cb2b
Matthias Scholz <matthias.scholz@thoughtworks.com>
2024-07-22 08:43:11 +0200

    :sparkles: golang profiling

--- flakes/go/flake.nix
+++ flakes/go/flake.nix
@@ -4,39 +4,53 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       goVersion = 21;
       overlays = [ (final: prev: { go = prev."go_1_${toString goVersion}"; }) ];
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit overlays system; };
-      });
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system: f { pkgs = import nixpkgs { inherit overlays system; }; }
+        );
     in
     {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            # go (specified by overlay)
-            go
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              # go (specified by overlay)
+              go
 
-            # goimports, godoc, etc.
-            gotools
-            gotestsum
-            delve
-            # Emacs integration
-            gopls
-            gomodifytags
-            gotests
-            gore
+              # goimports, godoc, etc.
+              gotools
+              gotestsum
+              delve
+              # Emacs integration
+              gopls
+              gomodifytags
+              gotests
+              gore
 
-            # https://github.com/golangci/golangci-lint
-            golangci-lint
+              # https://github.com/golangci/golangci-lint
+              golangci-lint
 
-            # go-template based generator
-            gotemplate
-          ];
-        };
-      });
+              # go-template based generator
+              gotemplate
+
+              # profiling
+              pprof
+            ];
+          };
+        }
+      );
     };
 }

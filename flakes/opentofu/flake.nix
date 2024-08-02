b004b51497d570c9862420f76c858c1ff2ce417c
Matthias Scholz <matthias.scholz@thoughtworks.com>
2024-08-02 11:01:24 +0200

    :sparkles: opentofu - infrastructure as code tooling

---
+++ flakes/opentofu/flake.nix
@@ -0,0 +1,35 @@
+# NOTE: Copy of https://github.com/the-nix-way/dev-templates/blob/main/hashi/flake.nix
+{
+  description = "A Nix-flake-based development environment for Terraform fork: OpenTofu";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f: nixpkgs.lib.genAttrs supportedSystems (system: f { pkgs = import nixpkgs { inherit system; }; });
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [ opentofu ];
+
+            # NOTE Not supported by direnv!
+            # https://discourse.nixos.org/t/how-to-define-alias-in-shellhook/15299
+            #shellHook = ''
+            #    alias tmg="terramate generate"
+            #'';
+          };
+        }
+      );
+    };
+}

3831e4ef998444c6554ca81382e47f11245a8437
Matthias Scholz <matthias.scholz@thoughtworks.com>
2025-04-29 16:07:48 +0200

    🐛 it seems there is a environment variable naming conflict

--- flakes/shell/flake.nix
+++ flakes/shell/flake.nix
@@ -55,12 +55,12 @@
             # https://discourse.nixos.org/t/how-to-define-alias-in-shellhook/15299
             shellHook = ''
               echo "INFO :: Build risor with all available modules"
-              TMPDIR=$(mktemp -d)
-              echo ".build folder: $TMPDIR"
-              git clone https://github.com/risor-io/risor.git $TMPDIR
-              cd $TMPDIR
+              TMPDIR_RISOR=$(mktemp -d)
+              echo ".build folder: $TMPDIR_RISOR"
+              git clone https://github.com/risor-io/risor.git $TMPDIR_RISOR
+              cd $TMPDIR_RISOR
               go install -tags=aws,carbon,cli,jmespath,k8s,pgx,semver,s3fs,template,uuid .
-              rm -rf $TMPDIR
+              rm -rf $TMPDIR_RISOR
 
               echo "INFO :: Configure autocompletion for Bash"
               source <(risor completion bash)
